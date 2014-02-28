9f829cc90f8b4af764d2558a0ea826d7c7df7e97
andreas jansson <andreas.s.t.jansson@gmail.com>
2014-02-28 07:39:34 +0000

    bash fool

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -e
 
 [ -n "$CLUSTER" ] || (echo '$CLUSTER is not defined' && exit 1)
 
-garbd -a "gcomm://$CLUSTER?pc.wait_prim=no" -g "{$CLUSTER_NAME:=mycluster}"
+garbd -a "gcomm://$CLUSTER?pc.wait_prim=no" -g "${CLUSTER_NAME:=mycluster}"
